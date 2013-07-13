9a7f5a32641554fc633a2b02f2977b0f573e3d9e
Patrick Madden <maddenpj@gmail.com>
2013-07-13 14:44:32 -0500

    Updated with color

--- scrape_fm.js
+++ scrape_fm.js
@@ -17,6 +17,12 @@ https.get("https://data.cityofchicago.org/api/views/hu6v-hsqb/rows.json?accessTy
 
 });
 
+function isOpen(start, end) {
+  var newStart = new Date((new Date(start)).setYear(2013))
+  var newEnd = new Date((new Date(end)).setYear(2013))
+  var now = new Date()
+  return (newStart < now && now < newEnd);
+}
 
 function parse(json) {
   var geoJson = { type : "FeatureCollection", features: [] };
@@ -24,7 +30,16 @@ function parse(json) {
   for( var i in json.data ) {
     var row = json.data[i];
 
-    var color = "#00B200"
+
+    var start = Date.parse(row[13]);
+    var end = Date.parse(row[14]);
+
+    if( id == 0) {
+      end = (new Date()).setMonth(5);
+    }
+
+
+    var color = isOpen(start,end) ? "#00B200" : "#B20000";
     var lat = parseFloat(row[18]), long = parseFloat(row[19]);
     var geoRow = { type: "Feature", id: id, properties: {"marker-color":color}, geometry: { type: "Point", "coordinates" : [long, lat, 0.0] } };
 
