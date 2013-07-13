3c80182e14aa035679b1ea8a0a54d4e17192a15b
Patrick Madden <maddenpj@gmail.com>
2013-07-13 15:24:18 -0500

    Updated with color

--- scrape_bike.js
+++ scrape_bike.js
@@ -17,6 +17,19 @@ https.get("https://data.cityofchicago.org/api/views/cbyb-69xx/rows.json?accessTy
 
 });
 
+function colorToHex(color) {
+  var red = color.R, green = color.G, blue = color.B;
+  var rgb = blue | (green << 8) | (red << 16);
+  return ('#' + ("000000" + rgb.toString(16)).substr(-6)).toUpperCase();
+}
+
+function randColor() {
+  var max = 255, min = 0;
+  var r = Math.floor(Math.random() * (max - min + 1)) + min;
+  var g = Math.floor(Math.random() * (max - min + 1)) + min;
+  var b =Math.floor(Math.random() * (max - min + 1)) + min;
+  return colorToHex( {R: r, G: g, B: b});
+}
 
 function parse(json) {
   var geoJson = { type : "FeatureCollection", features: [] };
@@ -25,8 +38,8 @@ function parse(json) {
     var row = json.data[i];
 
     var lat = parseFloat(row[14]), long = parseFloat(row[15]);
-    var geoRow = { type: "Feature", id: id, geometry: { type: "Point", "coordinates" : [long, lat, 0.0] } };
-
+    var geoRow = { type: "Feature", id: id, properties:{}, geometry: { type: "Point", "coordinates" : [long, lat, 0.0] } };
+    geoRow.properties["marker-color"] = randColor();
     geoJson.features.push(geoRow);
     id++;
   }
